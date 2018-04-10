926d359bb499764d9dfd1a0dc764e9d4859ea6bf
youness <benhlalyounes@gmail.com>
2018-04-10 21:47:42 +0200

    this is a modification to get familiar with git

--- app/src/main/java/corp/andromeda/uness/urex/HomeActivity.java
+++ app/src/main/java/corp/andromeda/uness/urex/HomeActivity.java
@@ -19,6 +19,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this is a comments
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
