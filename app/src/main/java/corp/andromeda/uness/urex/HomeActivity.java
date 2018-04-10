ede0840ee25f4fe59ad7a67cb6a02353aff77a1f
youness <benhlalyounes@gmail.com>
2018-04-10 21:52:35 +0200

    this is a test from tortoise

--- app/src/main/java/corp/andromeda/uness/urex/HomeActivity.java
+++ app/src/main/java/corp/andromeda/uness/urex/HomeActivity.java
@@ -38,7 +38,7 @@ public class HomeActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+// this is shit
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
